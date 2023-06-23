0674e71f0d037c2ae116b6aade276da6d7dd5d8a
Atmo <skylandersmimi@gmail.com>
2023-06-22 22:24:19 -0400

    THE LNK CRATE SUCKS, and depth of 1 .lnk copies are working

--- src/copy_manager.rs
+++ src/copy_manager.rs
@@ -1,3 +1,5 @@
+use lnk::ShellLink;
+
 use crate::config::Config;
 
 use std::ffi::OsStr;
@@ -6,6 +8,13 @@ use std::error::Error;
 use std::io::Error as IoError;
 use std::path::PathBuf;
 
+/// Handles all the copying
+/// 
+/// # Usage
+/// ```
+/// let copy_manager = CopyManager::new(Config::build_from_args(env::args()));
+/// copy_manager.run();
+/// ```
 pub struct CopyManager {
     config: Config,
 }
@@ -34,9 +43,13 @@ impl CopyManager {
             let entry = entry?;
             let path = entry.path();
             let entry_name = path.file_name().unwrap();
-            let target_path = format!("{}\\{}", target, entry_name.to_string_lossy());
 
-            let is_link = path.extension().unwrap_or(&OsStr::new("")) == ".lnk";
+            let is_link = path.extension().unwrap_or(&OsStr::new("")) == "lnk";
+
+            let target_path = match is_link && !inside_link {
+                true => target.to_string(),
+                false => format!("{}\\{}", target, entry_name.to_string_lossy()),
+            };
     
             if path.is_dir() {
                 self.copy_directory(&path.to_string_lossy(), &target_path, inside_link)?;
@@ -58,13 +71,22 @@ impl CopyManager {
 
     fn copy_link(&self, link_path: &str, target: &str) -> Result<(), Box<dyn Error>> {
         let link_path = PathBuf::from(link_path);
-        
-        let refered_entry = link_path.read_link()?;
+
+        // I'll use unwrap because lnk::Error doesn't implement std::error::Error :facepalm:
+        // AND the the path to the refered file is in a private field and there is no getter
+        let refered_entry_path = dbg!(ShellLink::open(link_path.clone()).unwrap()
+            .link_info()
+            .clone().unwrap()
+            .local_base_path())
+            .clone()
+            .unwrap();
+
+        let refered_entry = PathBuf::from(refered_entry_path);
         let refered_entry_name = refered_entry.file_name().unwrap();
         let target_path = format!("{}\\{}", target, refered_entry_name.to_string_lossy());
 
         if refered_entry.is_dir() {
-            self.copy_directory(&refered_entry.to_string_lossy(), &target_path, true)?;
+            self.copy_directory(dbg!(&refered_entry.to_string_lossy()), dbg!(&target_path), true)?;
         } else if refered_entry.is_file() || refered_entry.is_symlink() {
             fs::copy(refered_entry.to_string_lossy().to_string(), target_path)?;
         } else {
